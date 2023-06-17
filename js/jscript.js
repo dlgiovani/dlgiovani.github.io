506f407e954e587fadc5c5025228263929dc5a27
Giovani Drosda Lima <gdrosdalima@gmail.com>
2023-06-17 01:57:31 -0300

    Minor changes

--- js/jscript.js
+++ js/jscript.js
@@ -5,3 +5,44 @@ function copyText() {
   
   alert("Copied the text: " + copyText.innerText);
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+  const textElements = ['I solve problems.', 'I create solutions.'];
+  const delay = 2000; // Delay between text changes in milliseconds
+  let currentIndex = 0;
+  let currentText = '';
+  let isDeleting = false;
+
+  function type() {
+    const currentTextIndex = currentIndex % textElements.length;
+    const fullText = textElements[currentTextIndex];
+
+    if (isDeleting) {
+      currentText = fullText.substring(0, currentText.length - 1);
+    } else {
+      currentText = fullText.substring(0, currentText.length + 1);
+    }
+
+    document.getElementById('solutionsHover').textContent = currentText;
+
+    let typingSpeed = 200; // Speed of typing characters in milliseconds
+
+    if (isDeleting) {
+      typingSpeed /= 2; // Speed up deletion
+    }
+
+    // Check if the current text is fully typed or deleted
+    if (!isDeleting && currentText === fullText) {
+      isDeleting = true;
+      typingSpeed = delay; // Pause before starting deletion
+    } else if (isDeleting && currentText === 'I') {
+      isDeleting = false;
+      currentIndex++;
+      typingSpeed = 400; // Delay before typing the next text
+    }
+
+    setTimeout(type, typingSpeed);
+  }
+
+  type();
+});
