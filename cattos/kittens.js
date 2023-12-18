203890daa6c94811b690f122162bf94e829675a7
Giovani Drosda Lima <gdrosdalima@gmail.com>
2023-12-18 16:43:52 -0300

    minor changes

--- cattos/kittens.js
+++ cattos/kittens.js
@@ -2,18 +2,15 @@ document.addEventListener('DOMContentLoaded', function(e) {
     e.preventDefault();
     for (let x = 50; x <= 1200; x += 50) {
         for (let y = 50; y <= 1200; y += 50) {
-            // Create image element
             let factor = x - y
             if (factor * factor > 40000) {
                 continue;
             }
             const img = document.createElement('img');
 
-            // Set image source using template literal
             img.src = `https://placekitten.com/${x}/${y}`;
             img.style = 'max-width: 100vw;';
 
-            // Append image to body
             document.body.appendChild(img);
         }
     }
